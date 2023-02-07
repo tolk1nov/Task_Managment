eda06fefae828d791ef3d7be3ddc39a7ab024a77
tolk1nov <sarvartolqinov13@gmail.com>
2023-02-07 17:05:43 +0500

    first

--- src/main/java/uz/pdp/service/cheked/ChekedServise.java
+++ src/main/java/uz/pdp/service/cheked/ChekedServise.java
@@ -1,6 +1,9 @@
 package uz.pdp.service.cheked;
 
 import uz.pdp.controller.adminControlller.AdminController;
+import uz.pdp.entity.user.User;
+import uz.pdp.entity.user.UserRole;
+import uz.pdp.repository.UserRepository;
 
 import java.util.Objects;
 
@@ -8,6 +11,8 @@ import static uz.pdp.utill.Utill.*;
 
 public class ChekedServise {
     AdminController adminController = new AdminController();
+    UserRepository userRepository = UserRepository.getInstance();
+
     public void chekEmployee() {
     }
 
@@ -16,14 +21,40 @@ public class ChekedServise {
         String email = strScan.nextLine();
         System.out.print("Password kiriting: ");
         String password = strScan.nextLine();
-        if(Objects.equals(email,adminEmail)&&
-            Objects.equals(password,adminPassword)){
+        if (Objects.equals(email, adminEmail) &&
+                Objects.equals(password, adminPassword)) {
             adminController.adminController();
-        }else {
+        } else {
             System.out.println("Email yoki password notog'ri\n");
         }
     }
 
     public void singUpUser() {
+
+        System.out.print("Ismingizni kiriting: ");
+        String name = strScan.nextLine();
+        System.out.print("Familiyangizni kiriting: ");
+        String lastname = strScan.nextLine();
+        System.out.print("Email kiriting: ");
+        String email = strScan.nextLine();
+        if (!chekEmail(email)) {
+            System.err.println("Notogri kiritdingiz!");
+            singUpUser();
+        }
+        System.out.print("Password kiriting: ");
+        String password = strScan.nextLine();
+
+        if (userRepository.isExist(email)) {
+            System.out.println("Bunday eamil bn User mavjud!");
+        } else {
+            User user = new User(name, lastname, email, password, UserRole.USER);
+            userRepository.addUser(user);
+            System.out.println("\nAdmin javobini kuting!\n");
+        }
+
+    }
+
+    public Boolean chekEmail(String email) {
+        return email.contains("@") && email.contains(".");
     }
 }
